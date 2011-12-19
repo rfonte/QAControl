f211bea0ca25e50a587ea63d943e6386b46d30ad
Rodrigo <rodrigo.fonte@yahoo.com.br>
2011-12-19 21:20:33 -0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QAControl::Application.config.secret_token = 'bf24e2e3b381336dbfc647c3d0824d736a8d0d988aeb19e5a7992e4d2d0d477718c49e974d3e556ad4aab29d0462c1cf17f96b02e0606762a0cb1a9ca68d6c86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+QAControl::Application.config.session_store :cookie_store, key: '_QAControl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# QAControl::Application.config.session_store :active_record_store
